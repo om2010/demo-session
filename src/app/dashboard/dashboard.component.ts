205862b9059a485a6ba71f8aae47fd95c7a70fc4
om2010 <sudhakardwivedi5@gmail.com>
2019-04-08 11:08:57 +0530

    2 files modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router, ActivatedRoute } from "@angular/router";
 import { DatadashboardService } from '../datadashboard.service';
+
+import { Http, RequestOptions } from "@angular/http";
 import { User } from '../User';
+import "rxjs/Rx";
 
 
 @Component({
@@ -12,9 +15,11 @@ import { User } from '../User';
 })
 
 export class DashboardComponent implements OnInit {
+  private sid: string;
+    public entries: Array<any>;
  users: User[];
 
-  constructor(private auth : AuthService,private router: Router, private datadashboardService: DatadashboardService) { }
+  constructor(private http: Http,private auth : AuthService,private router: Router, private datadashboardService: DatadashboardService, private route: ActivatedRoute) { }
   ngOnInit() {
      if(!this.auth.check_user_login()){
         this.router.navigate(['/signin']);
